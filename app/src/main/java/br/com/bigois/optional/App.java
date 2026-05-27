9f67aa262e59fffcbfe746a39469b95b1d1eb7a2
Guilherme Bigois <guilherme.bigois@sankhya.com.br>
2026-05-27 17:52:45 -0300

    feat: start project

---
+++ app/src/main/java/br/com/bigois/optional/App.java
@@ -0,0 +1,10 @@
+package br.com.bigois.optional;
+
+public class App {
+    public static void main(String[] args) {
+        Person person = new Person("Guilherme", "Bigois");
+        System.out.println("First name: " + person.getFirstName());
+        System.out.println("Middle name: " + person.getMiddleName().orElse(""));
+        System.out.println("Last Name: " + person.getLastName());
+    }
+}
---
+++ app/src/main/java/br/com/bigois/optional/Person.java
@@ -0,0 +1,82 @@
+package br.com.bigois.optional;
+
+import java.util.Optional;
+
+public class Person {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+
+    public Person(String firstName, String middleName, String lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
+
+    /* Constructor overloading */
+    public Person(String firstName, String lastName) {
+        /* Constructor chaining */
+        this(firstName, null, lastName);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Optional<String> getMiddleName() {
+        return Optional.ofNullable(middleName);
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((middleName == null) ? 0 : middleName.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (middleName == null) {
+            if (other.middleName != null)
+                return false;
+        } else if (!middleName.equals(other.middleName))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        return true;
+    }
+}
