6aa57bff74b5d839125d34cfec2259cc6b0b8b14
Guilherme Bigois <guilhermebigois@outlook.com>
2026-07-05 07:33:50 -0300

    feat: add console text input example

---
+++ app/src/main/java/br/com/bigois/scanner/ScannerApp.java
@@ -0,0 +1,18 @@
+package br.com.bigois.scanner;
+
+import java.util.Scanner;
+
+public class ScannerApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try (sc) {
+            System.out.println("What is your name?");
+            String name = sc.nextLine();
+            System.out.println("How old are you?");
+            int age = sc.nextInt();
+
+            System.out.println("My name is " + name + " and I am " + age + " years old");
+        }
+    }
+}
