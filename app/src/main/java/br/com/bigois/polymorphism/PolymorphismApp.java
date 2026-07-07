d6cd540b94535f75417ad39c6e41ed30e6bc2f3c
Guilherme Bigois <guilhermebigois@outlook.com>
2026-07-06 23:27:24 -0300

    feat: add polymorphism example

---
+++ app/src/main/java/br/com/bigois/polymorphism/PolymorphismApp.java
@@ -0,0 +1,42 @@
+package br.com.bigois.polymorphism;
+
+import java.math.BigDecimal;
+
+import br.com.bigois.interfaces.classicinterface.BankAccount;
+import br.com.bigois.interfaces.defaultmethod.BrokerAccount;
+import br.com.bigois.interfaces.defaultmethod.BusinessAccount;
+import br.com.bigois.interfaces.defaultmethod.PersonalAccount;
+
+public class PolymorphismApp {
+    public static void main(String[] args) {
+        PersonalAccount personalAccount = new PersonalAccount(1L);
+        BusinessAccount businessAccount = new BusinessAccount(2L);
+
+        testOperations(personalAccount);
+        testInvestment((BrokerAccount) personalAccount, new BigDecimal("1000"));
+
+        System.out.println("");
+
+        testOperations(businessAccount);
+        testInvestment((BrokerAccount) businessAccount, new BigDecimal("1000"));
+    }
+
+    // Polymorphism by accepting any SUBCLASS of BankAccount
+    public static void testOperations(BankAccount account) {
+        System.out.println("=".repeat(40));
+        System.out.println("Account ID: " + account.getId() + " | " + "Account Type: " + account.getAccountType());
+        System.out.println("=".repeat(40));
+
+        System.out.println("Balance before deposit: " + account.getBalance());
+        account.deposit(new BigDecimal("500"));
+        System.out.println("Balance after deposit: " + account.getBalance());
+        account.withdraw(new BigDecimal("200"));
+        System.out.println("Balance after withdrawal: " + account.getBalance());
+    }
+
+    // Polymorphism by accepting any class IMPLEMENTING BrokerAccount
+    public static void testInvestment(BrokerAccount account, BigDecimal amount) {
+        BigDecimal investedAmount = account.simulateInvestment(amount);
+        System.out.println("Investment return: " + investedAmount);
+    }
+}
