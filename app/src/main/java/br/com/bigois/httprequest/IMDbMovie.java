980b74f0856165908765ecd20dd7ce713d729f6d
Guilherme Bigois <guilhermebigois@outlook.com>
2026-07-11 17:40:25 -0300

    refactor: convert IMDbMovie to a record

--- app/src/main/java/br/com/bigois/httprequest/IMDbMovie.java
+++ app/src/main/java/br/com/bigois/httprequest/IMDbMovie.java
@@ -2,214 +2,25 @@ package br.com.bigois.httprequest;
 
 import com.google.gson.annotations.SerializedName;
 
-public class IMDbMovie {
-    @SerializedName("Title")
-    private String title;
-    @SerializedName("Year")
-    private int year;
-    @SerializedName("Rated")
-    private String rated;
-    @SerializedName("Released")
-    private String released;
-    @SerializedName("Runtime")
-    private String runtime;
-    @SerializedName("Genre")
-    private String genre;
-    @SerializedName("Director")
-    private String director;
-    @SerializedName("Writer")
-    private String writer;
-    @SerializedName("Actors")
-    private String actors;
-    @SerializedName("Plot")
-    private String plot;
-    @SerializedName("Language")
-    private String language;
-    @SerializedName("Country")
-    private String country;
-    @SerializedName("Awards")
-    private String awards;
-    @SerializedName("Poster")
-    private String poster;
-    @SerializedName("Metascore")
-    private String metascore;
-    private String imdbRating;
-    private String imdbVotes;
-    private String imdbID;
-    @SerializedName("Type")
-    private String type;
-    private String totalSeasons;
-
-    public IMDbMovie() {
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public String getRated() {
-        return rated;
-    }
-
-    public void setRated(String rated) {
-        this.rated = rated;
-    }
-
-    public String getReleased() {
-        return released;
-    }
-
-    public void setReleased(String released) {
-        this.released = released;
-    }
-
-    public String getRuntime() {
-        return runtime;
-    }
-
-    public void setRuntime(String runtime) {
-        this.runtime = runtime;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public String getWriter() {
-        return writer;
-    }
-
-    public void setWriter(String writer) {
-        this.writer = writer;
-    }
-
-    public String getActors() {
-        return actors;
-    }
-
-    public void setActors(String actors) {
-        this.actors = actors;
-    }
-
-    public String getPlot() {
-        return plot;
-    }
-
-    public void setPlot(String plot) {
-        this.plot = plot;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getAwards() {
-        return awards;
-    }
-
-    public void setAwards(String awards) {
-        this.awards = awards;
-    }
-
-    public String getPoster() {
-        return poster;
-    }
-
-    public void setPoster(String poster) {
-        this.poster = poster;
-    }
-
-    public String getMetascore() {
-        return metascore;
-    }
-
-    public void setMetascore(String metascore) {
-        this.metascore = metascore;
-    }
-
-    public String getImdbRating() {
-        return imdbRating;
-    }
-
-    public void setImdbRating(String imdbRating) {
-        this.imdbRating = imdbRating;
-    }
-
-    public String getImdbVotes() {
-        return imdbVotes;
-    }
-
-    public void setImdbVotes(String imdbVotes) {
-        this.imdbVotes = imdbVotes;
-    }
-
-    public String getImdbID() {
-        return imdbID;
-    }
-
-    public void setImdbID(String imdbID) {
-        this.imdbID = imdbID;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getTotalSeasons() {
-        return totalSeasons;
-    }
-
-    public void setTotalSeasons(String totalSeasons) {
-        this.totalSeasons = totalSeasons;
-    }
-
-    @Override
-    public String toString() {
-        return "IMDbMovie [title=" + title + ", year=" + year + ", rated=" + rated + ", released=" + released
-                + ", runtime=" + runtime + ", genre=" + genre + ", director=" + director + ", writer=" + writer
-                + ", actors=" + actors + ", plot=" + plot + ", language=" + language + ", country=" + country
-                + ", awards=" + awards + ", poster=" + poster + ", metascore=" + metascore + ", imdbRating="
-                + imdbRating + ", imdbVotes=" + imdbVotes + ", imdbID=" + imdbID + ", type=" + type + ", totalSeasons="
-                + totalSeasons + "]";
-    }
+public record IMDbMovie(
+        @SerializedName("Title") String title,
+        @SerializedName("Year") int year,
+        @SerializedName("Rated") String rated,
+        @SerializedName("Released") String released,
+        @SerializedName("Runtime") String runtime,
+        @SerializedName("Genre") String genre,
+        @SerializedName("Director") String director,
+        @SerializedName("Writer") String writer,
+        @SerializedName("Actors") String actors,
+        @SerializedName("Plot") String plot,
+        @SerializedName("Language") String language,
+        @SerializedName("Country") String country,
+        @SerializedName("Awards") String awards,
+        @SerializedName("Poster") String poster,
+        @SerializedName("Metascore") String metascore,
+        String imdbRating,
+        String imdbVotes,
+        String imdbID,
+        @SerializedName("Type") String type,
+        String totalSeasons) {
 }
