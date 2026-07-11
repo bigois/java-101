f76ff1368b969df9b2273905388045366d48410d
Guilherme Bigois <guilhermebigois@outlook.com>
2026-07-11 20:23:38 -0300

    feat: add file read example

---
+++ app/src/main/java/br/com/bigois/io/simplewrite/SimpleWriteApp.java
@@ -0,0 +1,39 @@
+package br.com.bigois.io.simplewrite;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import br.com.bigois.httprequest.HttpRequestApp;
+
+public class SimpleWriteApp {
+    public static void main(String[] args) {
+        Path filePath = Paths.get("docs", "matrix.json");
+        File file = filePath.toFile();
+
+        // Ensure the parent directories exist before writing to the file
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+
+        // Check if the file already exists and print a message if it does
+        if (file.exists()) {
+            System.out.println("File already exists at " + file.getAbsolutePath());
+            System.exit(1);
+        }
+
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(getMatrixJson());
+            System.out.println("File written successfully to " + file.getAbsolutePath());
+        } catch (Exception e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+    }
+
+    public static String getMatrixJson() {
+        URI uri = HttpRequestApp.buildUri("Matrix");
+        return HttpRequestApp.sendRequest(uri);
+    }
+}
