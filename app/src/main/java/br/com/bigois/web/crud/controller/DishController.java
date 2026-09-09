813c11e0ee4ca5f2da1ee3c445a36690d547893a
Guilherme Bigois <guilhermebigois@outlook.com>
2026-09-09 17:31:35 -0300

    refactor: centralize API base path configuration

--- app/src/main/java/br/com/bigois/web/crud/controller/DishController.java
+++ app/src/main/java/br/com/bigois/web/crud/controller/DishController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v1/dishes", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/v1/dishes", produces = MediaType.APPLICATION_JSON_VALUE)
 public class DishController {
 	private final DishRepository dishRepository;
 
